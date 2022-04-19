f7f2c308599cc45cee5987673dc0500fafc1965c
Bruno Rocha <rochacbruno@gmail.com>
2022-04-20 00:36:53 +0100

    first

---
+++ beerlog/cli.py
@@ -0,0 +1,5 @@
+from .config import settings
+
+
+def main():
+    print("Hello from", settings.NAME)
---
+++ beerlog/config.py
@@ -0,0 +1,9 @@
+import os
+from dynaconf import Dynaconf
+
+
+settings = Dynaconf(
+    envvar_prefix="BEERLOG",
+    root_path=os.path.dirname(__file__),
+    settings_files=["settings.toml"],
+)
