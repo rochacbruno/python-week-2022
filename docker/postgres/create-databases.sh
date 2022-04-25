d5338592916eeb7956f4e26b5314290458fb0e5b
Bruno Rocha <rochacbruno@gmail.com>
2022-04-25 14:47:44 +0000

    Using compose + pgsql

---
+++ docker/postgres/create-databases.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -u
+
+function create_user_and_database() {
+	local database=$1
+	echo "Creating user and database '$database'"
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	    CREATE USER $database PASSWORD '$database';
+	    CREATE DATABASE $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+EOSQL
+}
+
+if [ -n "$POSTGRES_DBS" ]; then
+	echo "Creating DB(s): $POSTGRES_DBS"
+	for db in $(echo $POSTGRES_DBS | tr ',' ' '); do
+		create_user_and_database $db
+	done
+	echo "Multiple databases created"
+fi
