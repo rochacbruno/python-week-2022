02409f82380e7c5af58d30a8ae5c70e774007579
Andrei <infrati@infrati.dev>
2022-04-28 17:26:24 +0000

    feat: frontend cli

--- beerlog/cli.py
+++ beerlog/cli.py
@@ -1,5 +1,55 @@
-from .config import settings
+from typing import Optional
+
+import typer
+from rich import print  # NEW
+from rich.console import Console  # NEW
+from rich.table import Table  # NEW
+
+from beerlog.core import add_beer_to_database, get_beers_from_database
+
+main = typer.Typer(help="Beer Management Application")
+console = Console()
 
 
-def main():
-    print("Hello from", settings.NAME)
+@main.command()
+def add(
+    name: str,
+    style: str,
+    flavor: int = typer.Option(...),
+    image: int = typer.Option(...),
+    cost: int = typer.Option(...),
+):
+    """Adds a new beer to the database"""
+    if add_beer_to_database(name, style, flavor, image, cost):
+        print(":beer_mug: Beer added!!!")  # NEW
+    else:
+        print(":no_entry: - Cannot add beer.")  # NEW
+
+
+# NEW
+@main.command("list")
+def list_beers(style: Optional[str] = None):
+    """Lists beers from the database"""
+    beers = get_beers_from_database(style)
+    table = Table(
+        title="Beerlog Database :beer_mug:"
+        if not style
+        else f"Beerlog {style}"
+    )
+    headers = [
+        "id",
+        "name",
+        "style",
+        "flavor",
+        "image",
+        "cost",
+        "rate",
+        "date",
+    ]
+    for header in headers:
+        table.add_column(header, style="magenta")
+    for beer in beers:
+        beer.date = beer.date.strftime("%Y-%m-%d")
+        values = [str(getattr(beer, header)) for header in headers]
+        table.add_row(*values)
+    console.print(table)
