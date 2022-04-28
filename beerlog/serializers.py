076e31cab8b6c53ee4a753aa32d6e3fe148909ad
Andrei <infrati@infrati.dev>
2022-04-28 17:31:08 +0000

    feat:schemas to define the input and output serializers

---
+++ beerlog/serializers.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+from fastapi import HTTPException, status
+from pydantic import BaseModel, validator
+
+
+class BeerOut(BaseModel):
+    id: int
+    name: str
+    style: str
+    flavor: int
+    image: int
+    cost: int
+    rate: int
+    date: datetime
+
+
+class BeerIn(BaseModel):
+    name: str
+    style: str
+    flavor: int
+    image: int
+    cost: int
+
+    @validator("image", "flavor", "cost")
+    def validate_ratings(cls, v, field):
+        if v < 1 or v > 10:
+            raise HTTPException(
+                detail=f"{field.name} must be between 1 and 10",
+                status_code=status.HTTP_400_BAD_REQUEST,
+            )
+        return v
